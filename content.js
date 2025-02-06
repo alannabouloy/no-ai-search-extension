36d0d4132f369e8f970df1858804416270a750a4
Alanna Bouloy <lanihope@gmail.com>
2025-02-06 11:58:55 -0500

    NO-AI-1 set up file structure and create TODOs

---
+++ content.js
@@ -0,0 +1,6 @@
+//main file of extension
+//TODOS: 
+//check if extension is enabled
+//if so, check if on Google Search Page
+//if yes, then insert '-ai' into search bar automatically
+//create function that adds '-ai' to address bar searches as well
